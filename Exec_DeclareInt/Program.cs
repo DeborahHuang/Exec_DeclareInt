89f3779fb9bccd8b4c01561e52949c56a861ea0a
88693 <88693@LAPTOP-FS9ST1LD>
2022-10-18 20:52:04 +0800

    加入專案檔案。

---
+++ Exec_DeclareInt/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareInt
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//第一題:計算1+(2+2)*3-1
+			int count = 1 + (2 + 2) * 3 - 1;
+			Console.WriteLine(count);
+
+			//第二題:求120/7的商及餘數各是多少
+			int num1 = 120 / 7;
+			int num2 = 120 % 7;
+			Console.WriteLine(num1);
+			Console.WriteLine(num2);
+
+			//第三題:判斷9,202個別是奇數或偶數
+			//布林值寫法
+			int value = 9;
+			bool IsEvenInt = value % 2 == 0;
+			string reslut = IsEvenInt ? $"{value}偶數" : $"{value}為奇數";
+			Console.WriteLine(reslut);
+			
+			int value2 = 202;
+			string reslut2= IsEvenInt ? $"{value2}偶數" : $"{value2}為奇數" ;
+			Console.WriteLine(reslut2);
+			
+
+			//if寫法
+			if (value % 2 == 0)
+			{
+				Console.WriteLine($"{value}為偶數");
+			}
+			else
+			{
+				Console.WriteLine($"{value}為奇數");
+			}
+
+
+			if (value2 % 2 == 0)
+			{
+				Console.WriteLine($"{value2}為偶數");
+			}
+			else
+			{
+				Console.WriteLine($"{value2}為奇數");
+			}
+
+
+		}
+	}
+}
